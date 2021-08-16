5c0e7ec4ea4766f74e04e9986cc994c805e2cc8f
James Tang <james.tangsw@gmail.com>
2021-08-15 20:30:42 -0400

    solve with List instead of ListNode

---
+++ 2_Add Two Numbers.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+# Supposing it is list instead of ListNode
+# class Solution:
+#     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+#         l1.reverse()
+#         l2.reverse()
+l1 = [2,4,3]
+l2 = [5,6,4] 
+print(l1)
