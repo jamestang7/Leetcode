8616a5ba6a5d3c3dbd1afd5c54e7cc3e013e8886
James Tang <james.tangsw@gmail.com>
2022-01-22 18:35:41 -0500

    accepted solution

--- leetcode_q5.py
+++ leetcode_q5.py
@@ -1,54 +1,24 @@
-def lps(s):
-    n = len(s)
-    def Pal(str1):
-        if len(str1) == 0 or len(str1) == 1:
-            return True
-        elif str1[0] == str1[-1]:
-            return Pal(str1[1:-1])
-        else:
-            return False 
 
-    # Create a table to store results of subproblems
-    L = [[(0,(0,0)) for x in range(n)] for x in range(n)]
- 
-    # Strings of length 1 are palindrome of length 1
-    for i in range(n):
-        L[i][i] = (1,(0,0))
-
-    for cl in range(2, n+1):
-        for i in range(n-cl+1):
-            j = i+cl-1
-            if s[i] == s[j] and cl == 2:
-                L[i][j] = (2, (i, j))
-                
-            elif s[i] == s[j]:
-                a = L[i + 1][j]
-                b = L[i][j - 1]
-                c = L[i+1][j-1]
-                num_list = [i[0] for i in (a, b, c)]
-                cor_list = [i[1] for i in (a, b, c)]
-                map = dict(zip(num_list, cor_list))
-                if Pal(s[i: j + 1]):
-                    L[i][j] = (c[0] + 2, (i, j))
-                else:
-                    L[i][j] = (max(map.keys()), map[max(map.keys())])
-                
-            else:
-                a = L[i+1][j]
-                b = L[i][j-1]
-                if a[0] > b[0]:
-                    L[i][j] = (a[0], a[1])
-                else:
-                    L[i][j] = (b[0], b[1])
-            print(i,j, L[i][j])
-        print("------------")
-    length = L[0][n-1][0]
-    i, j = L[0][n-1][1]
-
-    sub = s[i: j + 1]
-    return sub
+def longestPalindrome(s):
+    result = ""
+    def helper(s, l, r):
+        # return the longest Pal from inner to outer
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return s[l + 1: r]
+    
+    for i in range(len(s)):
+        # odd case
+        temp = helper(s, i, i)
+        if len(temp) > len(result):
+            result = temp
+        temp = helper(s, i ,i + 1)
+        if len(temp) > len(result):
+            result = temp
+    return result 
 
 # Driver program to test above functions
 seq = "aacabdkacaa"
-print(lps(seq))
+print(longestPalindrome(seq))
 
